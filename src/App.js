51cb8bb418d159b3b1b6d6585df8b184f1d6dff9
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-06-12 21:35:15 -0500

    Add a website tagline

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <section className="landing-page">
       <h2>Random Cat Picture Generator</h2>
+      <p className="tagline">The Cutest Website You'll Visit Today</p>
       <button class="refresh-page-button" onClick={(() => window.location.reload())}>Show Me a New Cat!</button>
       <img className="cat-image" src="https://cataas.com/cat" alt="Randomly generated cat picture"></img>
     </section>
